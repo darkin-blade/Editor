ba006784457ad0ea1c2b8710072a8e37cd46df89
AAAatrox <2104934038@qq.com>
2019-09-06 21:04:14 +0800

    获取edittext内容

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -13,6 +13,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.helloworld.functions.FileManager;
@@ -73,8 +74,8 @@ public class MainActivity extends AppCompatActivity {
         testButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, MainActivity.this.getExternalFilesDir(".").getAbsolutePath(), Toast.LENGTH_LONG).show();
-                FileManager temp = new FileManager();
+                EditText text = findViewById(R.id.editText1);
+                Toast.makeText(MainActivity.this, text.getText().toString(), Toast.LENGTH_LONG).show();
             }
         });
     }
