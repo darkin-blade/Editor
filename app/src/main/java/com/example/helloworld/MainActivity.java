06aa51a83764e70a58748994b488b2ec5eaa7f1a
Lynx <2104934038@qq.com>
2019-09-09 16:41:44 +0800

    temp

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -251,16 +251,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {// TODO
 
-//                MyDialog dialog = new MyDialog(MainActivity.this, R.style.save_style);
-//                dialog.show();// TODO 点击事件,打开路径
+                MyDialog dialog = new MyDialog(MainActivity.this, R.style.save_style);
+                dialog.show();// TODO 点击事件,打开路径
 
-//                SharedPreferences preferences = getSharedPreferences("file_name", MODE_PRIVATE);// 只能被自己的应用程序访问
-//                SharedPreferences.Editor editor = preferences.edit();// 用于编辑存储数据
-//                Log.i("fuck data", preferences.getString("fuck", "nothing"));// 第二个参数:若找不到key,则返回第二个参数
+                SharedPreferences preferences = getSharedPreferences("file_name", MODE_PRIVATE);// 只能被自己的应用程序访问
+                SharedPreferences.Editor editor = preferences.edit();// 用于编辑存储数据
+                Log.i("fuck data", preferences.getString("fuck", "nothing"));// 第二个参数:若找不到key,则返回第二个参数
 
                 // 删除标签栏
                 Log.i("fuck before", file_cur_num + ", total: " + file_total_num);
-                if (file_cur_num >= 0) {// 当前是否打开了文件
+                if (file_cur_num >= 0 && false) {// 当前是否打开了文件
                     // 关闭当前页面
                     Button btn = findViewById(button_id + file_cur_num);// TODO 获取当前要关闭的页面tab
                     LinearLayout tab = findViewById(R.id.file_tab);
