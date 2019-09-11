cd3b2bfe1aadf98b63d0321e5595ec72fe5e3d7a
Lynx <2104934038@qq.com>
2019-09-11 10:26:06 +0800

    DialogFragment能够关闭输入法

--- app/src/main/java/com/example/helloworld/functions/MyWindow.java
+++ app/src/main/java/com/example/helloworld/functions/MyWindow.java
@@ -1,31 +1,26 @@
 package com.example.helloworld.functions;
 
-import android.app.Dialog;
-import android.content.Context;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.PopupWindow;
 
 import androidx.fragment.app.DialogFragment;
 
-import com.example.helloworld.MainActivity;
 import com.example.helloworld.R;
 
 public class MyWindow extends DialogFragment {
-    public Button yes;
+    public Button yes;// TODO
     public Button cancel;
     public Button no;
     public int result;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.manager_layout, container);
+        View view = inflater.inflate(R.layout.save_layout, container);
         getDialog().getWindow().setBackgroundDrawable(new ColorDrawable(0x00000000));
         return view;
     }
